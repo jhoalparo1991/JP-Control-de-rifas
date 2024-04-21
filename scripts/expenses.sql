0b3b821017cc0b7cf3bef5ee7c59a7d240a9dc82
jhonatan alberto padilla rovira <jhoalparo1991@gmail.com>
2024-04-21 10:22:54 -0500

    Update expenses.sql

--- scripts/expenses.sql
+++ scripts/expenses.sql
@@ -14,8 +14,7 @@ create table tbl_gastos(
 	referencia varchar(50) null,
 	descripcion varchar(255) not null,
 	valor decimal(18,2) not null,
-	fecha date not null,
-	hora time not null,
+	fecha date not null
 )
 go
 create proc sp_guardar_tipos_gastos
@@ -44,17 +43,35 @@ create proc sp_guardar_gastos
 @usuario_id int = 0,
 @referencia varchar(50) ='',
 @descripcion varchar(255) ='',
-@valor decimal(18,2) = 0.00
+@valor decimal(18,2) = 0.00,
+@fecha date = null
 as
 if @id <= 0
 begin
 	insert into tbl_gastos values(@tipo_gasto_id,@usuario_id,@referencia,@descripcion,
-								  @valor,getdate(),getdate())
+								  @valor,@fecha)
 end
 else
 begin
 	update tbl_gastos set tipo_gasto_id=@tipo_gasto_id,referencia=@referencia,
-						  descripcion=@descripcion, valor=@valor
+						  descripcion=@descripcion, valor=@valor, fecha=@fecha
 						  where id=@id
+end
+go
+create proc sp_mostrar_gastos
+as
+begin
+	select 
+		a.id,
+		a.tipo_gasto_id,
+		a.usuario_id,
+		b.codigo,
+		b.descripcion tipo_gasto,
+		a.referencia,
+		a.descripcion,
+		a.valor,
+		a.fecha
+	from tbl_gastos a inner join tbl_tipos_gastos b
+	on a.tipo_gasto_id = b.id
 end
 go
