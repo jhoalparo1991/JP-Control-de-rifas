ada98861843f7c01f43223a6e504b0d3d4ffebcc
jhonatan alberto padilla rovira <jhoalparo1991@gmail.com>
2024-04-21 12:59:28 -0500

    Update reporte_caja.sql

--- scripts/reporte_caja.sql
+++ scripts/reporte_caja.sql
@@ -1,5 +1,5 @@
-create proc sp_reporte_caja
-@fecha nchar(10)=''
+create proc sp_reporte_caja-- '04-20-2024'
+@fecha date
 as
 declare @total_abonos decimal(10,2) = ( select sum(valor_abono) from tbl_abonos_boleta where fecha_abono=@fecha)
 
@@ -16,11 +16,12 @@ group by forma_pago
 declare @total_comision_pagada decimal(10,2) = (select valor_pagado from tbl_pagos_abono_detalle where fecha_pago=@fecha)
 
 select 
-	@total_abonos as 'total_abonos', 
-	@total_comisiones as 'total_comisiones',
-	@total_gastos as 'total_gastos',
-	(@total_abonos - (@total_gastos + @total_comision_pagada )) as 'caja',
-	@total_comision_pagada as 'total_comision_pagada',
-	* from #tmp
+	case when @total_abonos is null then 0 else @total_abonos end as 'total_abonos', 
+	case when @total_comisiones is null then 0 else @total_comisiones end as 'total_comisiones',
+	case when @total_comision_pagada is null then 0 else @total_comision_pagada end as 'total_comision_pagada',
+	case when @total_gastos is null then 0 else @total_gastos end as 'total_gastos',
+	case when (@total_abonos - (@total_gastos + @total_comision_pagada )) is null then 0 else 
+	(@total_abonos - (@total_gastos + @total_comision_pagada )) end as 'caja'	
+	,* from #tmp
 
 drop table #tmp;
