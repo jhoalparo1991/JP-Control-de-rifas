5ce5850c12fcf73e5f270e96eec226622ab38a84
Jhonatan Padilla <jhoalparo1991gmail.com>
2024-12-15 14:21:55 -0500

    se agrega script sql con los ajustes en base de datos

--- scripts/15-12-2024-scripts.sql
+++ scripts/15-12-2024-scripts.sql
@@ -37,7 +37,69 @@ go
 alter table tbl_usuarios_permisos
 add desbloquear_boleta bit null
 go
-update tbl_usuarios_permisos set desbloquear_boleta=0
+alter table tbl_usuarios_permisos
+add ver_estado_boletas bit null
+go
+alter table tbl_usuarios_permisos
+add ver_caja bit null
+go
+alter table tbl_usuarios_permisos
+add ver_acumulado bit null
+go
+alter table tbl_usuarios_permisos
+add ver_todos_los_abonos bit null
+go
+alter table tbl_usuarios_permisos
+add ver_abonos_totales_por_clientes bit null
+go
+alter table tbl_usuarios_permisos
+add ver_comisiones_vendedores_pagada bit null
+go
+alter table tbl_usuarios_permisos
+add ver_abonos_por_vendedores bit null
+go
+alter table tbl_usuarios_permisos
+add ver_abonos_por_boletas bit null
+go
+alter table tbl_usuarios_permisos
+add ver_boletas_vendidas bit null
+go
+alter table tbl_usuarios_permisos
+add ver_formas_pago bit null
+go
+alter table tbl_usuarios_permisos
+add ver_filtrar_abonos_fecha_valor bit null
+go
+alter table tbl_usuarios_permisos
+add ver_comisiones_pagadas bit null
+go
+alter table tbl_usuarios_permisos
+add ver_gastos bit null
+go
+alter table tbl_usuarios_permisos
+add ver_reporte_abono_formas_pago bit null
+go
+alter table tbl_usuarios_permisos
+add ver_reporte_boletas_entre_fechas bit null
+go
+update tbl_usuarios_permisos set 
+desbloquear_boleta=0,
+ver_estado_boletas=0,
+ver_caja=0,
+ver_acumulado=0,
+ver_todos_los_abonos=0,
+ver_abonos_totales_por_clientes=0,
+ver_comisiones_vendedores_pagada=0,
+ver_abonos_por_vendedores=0,
+ver_abonos_por_boletas=0,
+ver_boletas_vendidas=0,
+ver_formas_pago=0,
+ver_filtrar_abonos_fecha_valor=0,
+ver_comisiones_pagadas=0,
+ver_gastos=0,
+ver_reporte_abono_formas_pago=0,
+ver_reporte_boletas_entre_fechas=0
+
 go
 ALTER proc [dbo].[sp_mostrar_reporte_total_caja]  
 as  
@@ -108,8 +170,148 @@ begin
 end
 select @total_abonos as abonos, @total_gastos as gastos, @total_comision_pagada as comision, @total_caja as caja 
 go
---select sum(valor_abono) abono, ((sum(valor_abono) * 30) / 100 ) comision
---from tbl_abonos_boleta
--- AGREGAR CAMPO al procedimiento - sp_guardar_usuarios
---@bloquear_desbloquear bit = 0
+ALTER proc [dbo].[sp_guardar_usuarios]
+@id int = 0,
+@nombre_completo varchar(100) = '',
+@nro_doc varchar(20) = '',
+@direccion varchar(50) = '',
+@celular varchar(20) = '',
+@telefono varchar(20) = '',
+@clave varchar(500) = '',
+@comision decimal(10,2) = 0.00,
+@activo bit = 0,
+-- permisos
+@vendedores bit = 0,
+@clientes bit = 0,
+@rifas bit = 0,
+@boletas bit = 0,
+@pago_comisiones bit = 0,
+@reportes bit = 0,
+@egresos bit = 0,
+@crear_tipo_egreso bit = 0,
+@borrar_egreso bit = 0,
+@editar_egreso bit = 0,
+@crear_egreso bit = 0,
+@imprimir_egreso bit = 0,
+@crear_copia_seguridad bit = 0,
+@registrar_abono bit = 0,
+@borrar_abono bit = 0,
+@cambiar_fp_abono bit = 0,
+@cambiar_cliente_abono bit = 0,
+-- permisos-adicionales
+@crear_vendedores bit=0,
+@editar_vendedores bit=0,
+@borrar_vendedores bit=0,
+@asignar_boletas_vendedores bit=0,
+@ver_log bit = 0,
+@ver_boleta bit = 0,
+@bloquear_desbloquear bit = 0,
+@ver_estado_boletas bit = 0,
+@ver_caja bit = 0,
+@ver_acumulado bit = 0,
+@ver_todos_los_abonos bit = 0,
+@ver_abonos_totales_por_clientes bit = 0,
+@ver_comisiones_vendedores_pagada bit = 0,
+@ver_abonos_por_vendedores bit = 0,
+@ver_abonos_por_boletas bit = 0,
+@ver_boletas_vendidas bit = 0,
+@ver_formas_pago bit = 0,
+@ver_filtrar_abonos_fecha_valor bit = 0,
+@ver_comisiones_pagadas bit = 0,
+@ver_gastos bit = 0,
+@ver_reporte_abono_formas_pago bit = 0,
+@ver_reporte_boletas_entre_fechas bit = 0
+as
+begin try
+	begin tran
+		if @id <= 0
+			begin
+
+				if exists(select * from tbl_usuarios where nro_doc=@nro_doc)
+				raiserror('Ya existe un usuario un usuario con este número de documento',16,1)
+				else
+				declare @usuarioId int
+				insert into tbl_usuarios values(@nombre_completo,
+												@nro_doc,
+												@direccion,
+												@celular,
+												@telefono,
+												@clave,
+												@comision,
+												@activo)
+
+				set @usuarioId = @@IDENTITY
+
+				insert into tbl_usuarios_permisos values(@usuarioId,@vendedores,@clientes,@rifas,@boletas,@pago_comisiones,
+					@reportes,@egresos,@crear_tipo_egreso,@borrar_egreso,@editar_egreso,						@crear_egreso,@imprimir_egreso,@crear_copia_seguridad,@registrar_abono,
+					@borrar_abono,@cambiar_fp_abono,@cambiar_cliente_abono,@crear_vendedores,
+					@editar_vendedores,@borrar_vendedores,@asignar_boletas_vendedores,@ver_log,@ver_boleta,@bloquear_desbloquear,
+					@ver_estado_boletas,@ver_caja,@ver_acumulado,@ver_todos_los_abonos,@ver_abonos_totales_por_clientes,
+					@ver_comisiones_vendedores_pagada,@ver_abonos_por_vendedores,@ver_abonos_por_boletas,@ver_boletas_vendidas,
+					@ver_formas_pago,@ver_filtrar_abonos_fecha_valor,@ver_comisiones_pagadas,@ver_gastos,@ver_reporte_abono_formas_pago,
+					@ver_reporte_boletas_entre_fechas)
+
+			end
+			else
+			begin
+				if exists(select * from tbl_usuarios where nro_doc=@nro_doc and id<>@id)
+				raiserror('Ya existe un usuario un usuario con este número de documento',16,1)
+				else
+				update tbl_usuarios set 		nombre_completo=@nombre_completo,
+												nro_doc=@nro_doc,
+												direccion=@direccion,
+												telefono=@telefono,
+												comision=@comision,
+												activo=@activo,
+												clave=@clave
+												where id=@id
+
+				update tbl_usuarios_permisos set vendedores=@vendedores,
+												clientes=@clientes,
+												rifas=@rifas,
+												boletas=@boletas,
+												pago_comisiones=@pago_comisiones,
+												reportes=@reportes,
+												egresos=@egresos,
+												crear_tipo_egreso=@crear_tipo_egreso,
+												borrar_egreso=@borrar_egreso,
+												editar_egreso=@editar_egreso,
+												crear_egreso=@crear_egreso,
+												imprimir_egreso=@imprimir_egreso,
+												crear_copia_seguridad=@crear_copia_seguridad,
+												registrar_abono=@registrar_abono,
+												borrar_abono=@borrar_abono,
+												cambiar_fp_abono=@cambiar_fp_abono,
+												cambiar_cliente_abono=@cambiar_cliente_abono,
+												crear_vendedores=@crear_vendedores,
+												editar_vendedores=@editar_vendedores,
+												borrar_vendedores=@borrar_vendedores,
+												asignar_boletas_vendedores=@asignar_boletas_vendedores,
+												ver_log=@ver_log,
+												ver_boleta=@ver_boleta,
+												desbloquear_boleta=@bloquear_desbloquear,
+												ver_estado_boletas = @ver_estado_boletas,
+												ver_caja = @ver_caja,
+												ver_acumulado = @ver_acumulado,
+												ver_todos_los_abonos = @ver_todos_los_abonos,
+												ver_abonos_totales_por_clientes = @ver_abonos_totales_por_clientes,
+												ver_comisiones_vendedores_pagada = @ver_comisiones_vendedores_pagada,
+												ver_abonos_por_vendedores = @ver_abonos_por_vendedores,
+												ver_abonos_por_boletas = @ver_abonos_por_boletas,
+												ver_boletas_vendidas = @ver_boletas_vendidas,
+												ver_formas_pago = @ver_formas_pago,
+												ver_filtrar_abonos_fecha_valor = @ver_filtrar_abonos_fecha_valor,
+												ver_comisiones_pagadas = @ver_comisiones_pagadas,
+												ver_gastos = @ver_gastos,
+												ver_reporte_abono_formas_pago = @ver_reporte_abono_formas_pago,
+												ver_reporte_boletas_entre_fechas = @ver_reporte_boletas_entre_fechas
+												where usuario_id=@id
+			end
+	commit
+end try
+begin catch
+	rollback
+	print error_message()
+end catch
+go
 
